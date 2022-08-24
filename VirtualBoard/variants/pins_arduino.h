f3edc5b995cb4a138162b542461079168b7aafd0
Virtual Maker <56232456+virtual-maker@users.noreply.github.com>
2022-08-25 01:03:08 +0200

    Add RP2040 to variants

--- VirtualBoard/variants/pins_arduino.h
+++ VirtualBoard/variants/pins_arduino.h
@@ -7,7 +7,7 @@
   Optionally, real binary and analogue I/O pins as well as I2C and SPI interfaces
   can be controlled via an IO-Warrior device.
   https://github.com/virtual-maker/VirtualBoard
-
+  
   Created by Immo Wache <virtual.mkr@gmail.com>
   Copyright (c) 2022 Immo Wache. All right reserved.
 
@@ -322,9 +322,9 @@ static const uint8_t A5 = 5;
 #define NUM_DIGITAL_PINS        48
 #define NUM_ANALOG_INPUTS       20
 
-#define analogInputToDigitalPin(p)  (((p)<20)?(analogChannelToDigitalPin(p)):-1)
-#define digitalPinToInterrupt(p)    (((p)<48)?(p):-1)
-#define digitalPinHasPWM(p)         (p < 46)
+#define analogInputToDigitalPin(p)  (((p) < NUM_ANALOG_INPUTS) ? (analogChannelToDigitalPin(p)) : -1)
+#define digitalPinToInterrupt(p)    (((p) < NUM_DIGITAL_PINS) ? (p) : -1)
+#define digitalPinHasPWM(p)         (p < EXTERNAL_NUM_INTERRUPTS)
 
 static const uint8_t LED_BUILTIN = 15;
 #define BUILTIN_LED  LED_BUILTIN // backward compatibility
@@ -585,6 +585,98 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 #define MAX_NB_PORT 6
 #define A0 PA0
 #define MAX_SERVOS 12
+
+//-------------------------------------------------------------------------------
+
+#elif defined(ARDUINO_ARCH_RP2040)
+
+// Seeed XIAO RP2040
+#if defined(ARDUINO_SEEED_XIAO_RP2040)
+// LEDs
+#define PIN_LED        (17u)
+#define PIN_LED_R      (17u)
+#define PIN_LED_G      (16u)
+#define PIN_LED_B      (25u)
+#define LED_BUILTIN    PIN_LED
+
+// Digital pins
+static const uint8_t D0 = (26u);
+static const uint8_t D1 = (27u);
+static const uint8_t D2 = (28u);
+static const uint8_t D3 = (29u);
+static const uint8_t D4 = (6u);
+static const uint8_t D5 = (7u);
+static const uint8_t D6 = (0u);
+static const uint8_t D7 = (1u);
+static const uint8_t D8 = (2u);
+static const uint8_t D9 = (4u);
+static const uint8_t D10 = (3u);
+
+// Analog pins
+static const uint8_t A0 = (26u);
+static const uint8_t A1 = (27u);
+static const uint8_t A2 = (28u);
+static const uint8_t A3 = (29u);
+#define ADC_RESOLUTION 12
+
+// NeoPixel
+#define PIN_NEOPIXEL   (12u)
+#define NEOPIXEL_POWER (11u)
+
+// Serial1
+#define PIN_SERIAL1_TX (0u)
+#define PIN_SERIAL1_RX (1u)
+
+// Serial2 not pinned out
+#define PIN_SERIAL2_TX (31u)
+#define PIN_SERIAL2_RX (31u)
+
+// SPI
+#define PIN_SPI0_MISO  (4u)
+#define PIN_SPI0_MOSI  (3u)
+#define PIN_SPI0_SCK   (2u)
+#define PIN_SPI0_SS    (31u) // not pinned out
+static const uint8_t SS = PIN_SPI0_SS;   // SPI Slave SS not used. Set here only for reference.
+static const uint8_t MOSI = PIN_SPI0_MOSI;
+static const uint8_t MISO = PIN_SPI0_MISO;
+static const uint8_t SCK = PIN_SPI0_SCK;
+
+// Not pinned out
+#define PIN_SPI1_MISO  (31u)
+#define PIN_SPI1_MOSI  (31u)
+#define PIN_SPI1_SCK   (31u)
+#define PIN_SPI1_SS    (31u)
+#define SPI_MISO       (PIN_SPI1_MISO)
+#define SPI_MOSI       (PIN_SPI1_MOSI)
+#define SPI_SCK        (PIN_SPI1_SCK)
+
+// Wire
+#define __WIRE0_DEVICE (i2c1)
+#define PIN_WIRE0_SDA  (6u)
+#define PIN_WIRE0_SCL  (7u)
+#define SDA            PIN_WIRE0_SDA
+#define SCL            PIN_WIRE0_SCL
+#define I2C_SDA        (SDA)
+#define I2C_SCL        (SCL)
+
+// Wire1 not pinned out
+#define __WIRE1_DEVICE (i2c0)
+#define PIN_WIRE1_SDA  (31u)
+#define PIN_WIRE1_SCL  (31u)
+
+#define SERIAL_HOWMANY (1u)
+#define SPI_HOWMANY    (1u)
+#define WIRE_HOWMANY   (1u)
+
+// Raspberry Pi Pico
+#elif defined(ARDUINO_RASPBERRY_PI_PICO)
+// ToDo: add pin description here
+
+#else
+#error Unknown board with Raspberry Pico 2040 architecture
+#endif
+
+//-------------------------------------------------------------------------------
 
 // anything else
 #else
